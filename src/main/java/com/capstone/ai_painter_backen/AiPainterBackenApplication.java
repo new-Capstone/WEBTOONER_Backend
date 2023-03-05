34f2a561e2ae0e670526a6897efbf03c3b564261
JinEunPark <wlsdmsrnfl@naver.com>
2023-03-05 20:38:19 +0900

    init project welcome

---
+++ src/main/java/com/capstone/ai_painter_backen/AiPainterBackenApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.ai_painter_backen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPainterBackenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPainterBackenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/ai_painter_backen/AiPainterBackenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.ai_painter_backen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPainterBackenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
