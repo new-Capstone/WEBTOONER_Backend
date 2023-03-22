2af2df9456ae1d6b6ca105e3e6a1e91f7ecb9360
jinsu868 <mjinsu098@gmail.com>
2023-03-23 01:53:26 +0900

    remove : tutorRepository.java

--- src/main/java/com/capstone/ai_painter_backen/repository/TutorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.ai_painter_backen.repository;
-
-import com.capstone.ai_painter_backen.domain.mentor.TutorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TutorRepository extends JpaRepository<TutorEntity, Long> {
-}
