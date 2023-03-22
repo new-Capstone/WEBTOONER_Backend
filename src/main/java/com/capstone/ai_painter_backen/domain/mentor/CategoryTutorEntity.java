ea2dacf326186fcd035beda384a1b32b2023ed60
jinsu868 <mjinsu098@gmail.com>
2023-03-23 00:35:48 +0900

    feat : CategoryTutor 생성 메서드 추가

--- src/main/java/com/capstone/ai_painter_backen/domain/mentor/CategoryTutorEntity.java
+++ src/main/java/com/capstone/ai_painter_backen/domain/mentor/CategoryTutorEntity.java
@@ -2,10 +2,16 @@ package com.capstone.ai_painter_backen.domain.mentor;
 
 import com.capstone.ai_painter_backen.domain.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryTutorEntity extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +24,17 @@ public class CategoryTutorEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tutor_id")
     private TutorEntity tutorEntity;
+
+    /*
+    생성 메서드 : tutor 생성 시 호출
+     */
+    
+    public static CategoryTutorEntity createCategoryTutor(CategoryEntity category, TutorEntity tutor) {
+        CategoryTutorEntity categoryTutor = CategoryTutorEntity.builder()
+                .tutorEntity(tutor)
+                .categoryEntity(category)
+                .build();
+
+        return categoryTutor;
+    }
 }
