7629037bc90040eaea7a12a17aabeafdea5293f1
gujaeseong <gjs5448@gmail.com>
2023-03-23 14:49:57 +0900

    Room dto 작성

---
+++ src/main/java/com/capstone/ai_painter_backen/dto/RoomDto.java
@@ -0,0 +1,54 @@
+package com.capstone.ai_painter_backen.dto;
+
+import com.capstone.ai_painter_backen.domain.UserEntity;
+import com.capstone.ai_painter_backen.domain.message.MessageEntity;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomDto {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class PostDto {
+        private UserEntity owner;
+        private UserEntity visitor;
+        private List<MessageEntity> messageEntities = new ArrayList<>();
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class ResponseDto {
+        private Long roomid;
+        private UserEntity owner;
+        private UserEntity visitor;
+        private List<MessageEntity> messageEntities = new ArrayList<>();
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class DeleteDto {
+        private Long roomid;
+    }
+
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class PatchDto {
+        private Long roomid;
+        private List<MessageEntity> messageEntities = new ArrayList<>();
+    }
+}
