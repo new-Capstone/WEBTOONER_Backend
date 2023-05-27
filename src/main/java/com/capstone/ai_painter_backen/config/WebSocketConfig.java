c9f56b114ebeaf22bfa4e409ec8d7f45e56192eb
jinsu868 <mjinsu098@gmail.com>
2023-05-27 23:39:26 +0900

    [feat] 채팅, 알림, 기능 api 테스트(local) 완료, SecurityConfig 수정
    1. 채팅 보안 제거하고 테스트 진행 -> pass (interceptor로 따로 처리할 예정)
    2. 채팅 테스트 중 발생한 CORS error fix

--- src/main/java/com/capstone/ai_painter_backen/config/WebSocketConfig.java
+++ src/main/java/com/capstone/ai_painter_backen/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.capstone.ai_painter_backen.config;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,12 +11,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    //private final ChatPreHandler chatPreHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //client connection
         registry.addEndpoint("/ws-stomp")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
@@ -22,4 +29,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/pub");
         registry.enableSimpleBroker("/sub");
     }
+
+    /* interceptor 에서 처리 (Filter 넘기고)
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(chatPreHandler);
+    }
+    */
+
 }
