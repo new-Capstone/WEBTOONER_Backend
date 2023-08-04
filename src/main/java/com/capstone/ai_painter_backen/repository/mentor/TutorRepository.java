eb5b467ddceb320d5c74e1abfa727a3a93645777
JinEunPark <wlsdmsrnfl@naver.com>
2023-08-04 16:07:44 +0900

    [진은] 쿼리 변경

--- src/main/java/com/capstone/ai_painter_backen/repository/mentor/TutorRepository.java
+++ src/main/java/com/capstone/ai_painter_backen/repository/mentor/TutorRepository.java
@@ -1,7 +1,10 @@
 package com.capstone.ai_painter_backen.repository.mentor;
 
 import com.capstone.ai_painter_backen.domain.mentor.TutorEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.beans.JavaBean;
 
